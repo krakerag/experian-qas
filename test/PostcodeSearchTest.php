08b38757217b0090519a5f41a3e6ef9fab9ad0f8
Matthew Hallsworth <matthew@mhalls.net>
2014-03-04 23:40:24 +1100

    Improved test coverage
    Implemented some improved testing to ensure that we can catch the full extent of the SOAP request.

--- test/PostcodeSearchTest.php
+++ test/PostcodeSearchTest.php
@@ -23,28 +23,115 @@ class PostcodeSearchTest extends \PHPUnit_Framework_TestCase {
         }
     }
 
-    public function testSetGetSoapClient()
+    public function testGetEmptyMockFromWsdl()
     {
         $wsdl = __DIR__.'/default.wsdl';
+
+        $mockObject = $this->getMockFromWsdl($wsdl, 'ProWeb');
+        $mockObject->expects($this->any())
+            ->method('DoSearch')
+            ->will($this->returnValue($this->standardReturnEmptyObject()));
 
         $engine = new Engine();
         $search = new PostcodeSearch(new \Psr\Log\NullLogger(), $wsdl);
         $search->setEngine($engine);
+        $search->setSoapClient($mockObject);
 
-        // Create mock object for SoapClient
-        $mockSoapClient = $this->getMockBuilder('SoapClient')
-            ->setMethods(array('DoSearch'))
-            ->disableOriginalConstructor()
-            ->getMock();
-        $mockSoapClient->expects($this->once())->method('DoSearch')->will($this->returnValue(array()));
-
-        try {
-            $mockSoapClient->DoSearch(array());
-
-        } catch (\Exception $exception) {
-            $this->fail('For some reason the mock soap object returned something other than an array()');
-        }
+        $response = $search->find('GBR','AB123');
+        $this->assertNull($response, 'No postcode information returned so should get a null');
     }
 
+    public function testGetSingleResult()
+    {
+        $wsdl = __DIR__.'/default.wsdl';
+
+        $mockObject = $this->getMockFromWsdl($wsdl, 'ProWeb');
+        $mockObject->expects($this->any())
+            ->method('DoSearch')
+            ->will($this->returnValue($this->standardReturnValidSingleObject()));
+
+        $engine = new Engine();
+        $search = new PostcodeSearch(new \Psr\Log\NullLogger(), $wsdl);
+        $search->setEngine($engine);
+        $search->setSoapClient($mockObject);
+
+        $result = $search->find('GBR','AB123');
+        $this->assertCount(0, $result, 'Should get an empty array');
+
+    }
+
+    public function testGetMultipleResult()
+    {
+        $wsdl = __DIR__.'/default.wsdl';
+
+        $mockObject = $this->getMockFromWsdl($wsdl, 'ProWeb');
+        $mockObject->expects($this->any())
+            ->method('DoSearch')
+            ->will($this->returnValue($this->standardReturnValidMultipleObjects()));
+
+        $engine = new Engine();
+        $search = new PostcodeSearch(new \Psr\Log\NullLogger(), $wsdl);
+        $search->setEngine($engine);
+        $search->setSoapClient($mockObject);
+
+        $result = $search->find('GBR','AB123');
+        $this->assertCount(3, $result, 'Should get an empty array');
+
+    }
+
+    /**
+     * Test stub for returning an empty dataless object
+     *
+     * @return \stdClass
+     */
+    private function standardReturnEmptyObject()
+    {
+        $response = new \stdClass();
+        $response->QAPicklist = array();
+
+        return $response;
+    }
+
+    /**
+     * Test stub for returning a single address
+     *
+     * @return \stdClass
+     */
+    private function standardReturnValidSingleObject()
+    {
+        $response = new \stdClass();
+
+        $qaPickList = new \stdClass();
+        $qaPickList->Moniker = "";
+        $qaPickList->Postcode = "";
+
+        $response->QAPicklist = array($qaPickList);
+
+        return $response;
+    }
+
+    /**
+     * Test stub for returning a multiple address object
+     *
+     * @return \stdClass
+     */
+    private function standardReturnValidMultipleObjects()
+    {
+        $response = new \stdClass();
+
+        $qaPickList = new \stdClass();
+        $qaPickList->Moniker = "Multiple matches";
+        $qaPickList->Postcode = "1234";
+
+        $response->QAPicklist = array(
+            array(
+                'address1' => "Test address 1",
+                'address2' => "Test address 2",
+                'address3' => "Test address 3",
+            )
+        );
+
+        return $response;
+    }
 
 }
